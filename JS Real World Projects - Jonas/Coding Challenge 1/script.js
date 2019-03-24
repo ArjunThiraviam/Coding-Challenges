dac778b4c9fe45b1b47d0364957c50ac70837546
athiraviam <athiraviam@lenovo.com>
2019-03-24 19:53:18 +0530

    Code Challenge 1 : Jonas - The Complete JS 2019 Real World Projects

---
+++ JS Real World Projects - Jonas/Coding Challenge 1/script.js
@@ -0,0 +1,29 @@
+//**********************
+// Step 1
+//**********************
+var arjunMass = 68;
+var suriyaMass = 62;
+
+var arjunHeight =1.68;
+var suriyaHeight = 1.68;
+
+
+//**********************
+// Step 2
+//**********************
+var arjunBmi = arjunMass/ (arjunHeight*arjunHeight);
+var suriyaBmi = suriyaMass/ (suriyaHeight*suriyaHeight);
+
+console.log(arjunBmi, suriyaBmi);
+
+//**********************
+// Step 3
+//**********************
+
+var isArjunBMIMore = arjunBMI > suriyaBMI;
+
+//**********************
+// Step 4
+//**********************
+
+console.log(isArjunBMIMore);
